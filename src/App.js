5f4012e5ab4fafedda5ecec9c082b73231438239
kanyapandey <kanyapandey2603@gmail.com>
2019-05-04 22:06:13 +0700

    Add some css

--- src/App.js
+++ src/App.js
@@ -1,99 +1,103 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
   constructor(props){
-    super(props);
-    
-    this.state = {
-      username : ''
-    }
-    
-    this.updateInput = this.updateInput.bind(this);
-    }
-    
-    
-    updateInput(event){
-      var amount = this.nameTextInput.value;
-    var dollars; 
-    var quarters; 
-    var dimes; 
-    var nickels; 
-    var pennies; 
-    
-    var dollarRem; 
-    var quarterRem; 
-    var dimeRem; 
-    var nickelRem;
-    
-    var final = []
-    var dollarsbill
-    var output=""
-    
-    amount = amount * 100;
-    dollars = Math.floor (amount / 100);
-    dollarRem = amount % 100; 
-    quarters = Math.floor (dollarRem / 25);
-    quarterRem = dollarRem % 25;
-    
-    dimes = Math.floor (quarterRem / 10);
-    dimeRem = quarterRem % 10;
-    nickels = Math.floor (dimeRem / 5);
-    nickelRem = dimeRem % 5;
-    pennies = nickelRem;
-    
-    while(dollars>0){
-      if(parseInt(dollars)>=100){
-        dollarsbill = 100
-        final.push(dollarsbill)
-        dollars = dollars%dollarsbill
-      }else if(parseInt(dollars)>=50){
-        dollarsbill = 50
-        final.push(dollarsbill)
-        dollars = dollars%dollarsbill
-      }else if(parseInt(dollars)>=20){
-        dollarsbill = 20
-        final.push(dollarsbill)
-        dollars = dollars%dollarsbill
-      }else if(parseInt(dollars)>=10){
-        dollarsbill = 10
-        final.push(dollarsbill)
-        dollars = dollars%dollarsbill
-      }else if(parseInt(dollars)>=5){
-        dollarsbill = 5
-        final.push(dollarsbill)
-        dollars = dollars%dollarsbill
-      }else{
-          final.push(dollars)
-          break;
+      super(props);
+      this.state = {
+        result : ''
       }
+      this.calculateChange = this.calculateChange.bind(this);
     }
-    for(var i=0;i<final.length; i++){
-      output += `1 ${final[i]} dollar bill, `
-    }
-    if(quarters||dimes||nickels||pennies){
-      output += `${parseInt(quarters)} quarters, ${parseInt(dimes)} dimes, ${parseInt(nickels)} nickles, ${parseInt(pennies)} pennies`
-    }
-    console.log(output)
-    this.setState({username : output})
+    
+    
+    calculateChange(event){
+      var amount = this.valueDollar.value;
+      var dollars,quarters,dimes,nickels,pennies,dollarRem,quarterRem,dimeRem,nickelRem,dollarsbill,r,b
+      
+      var final = []
+      var output=""
+  
+      amount = amount * 100;
+      dollars = Math.floor (amount / 100);
+      dollarRem = amount % 100; 
+      quarters = Math.floor (dollarRem / 25);
+      quarterRem = dollarRem % 25;
+      
+      dimes = Math.floor (quarterRem / 10);
+      dimeRem = quarterRem % 10;
+      nickels = Math.floor (dimeRem / 5);
+      nickelRem = dimeRem % 5;
+      pennies = nickelRem;
+      
+      while(dollars>0){
+        if(dollars>=100){
+          dollarsbill = 100
+          b = parseInt(dollars/100)
+          for(var i=0;i<b;i++){
+            final.push(dollarsbill)
+          }
+          r = parseInt(dollars%100)
+          if(r!=0){
+            dollars = r
+          }else{
+            break;
+          }
+          
+        }else if(parseInt(dollars)>=50){
+          dollarsbill = 50
+          final.push(dollarsbill)
+          dollars = dollars-dollarsbill
+        }else if(parseInt(dollars)>=20){
+          dollarsbill = 20
+          final.push(dollarsbill)
+          dollars = dollars-dollarsbill
+        }else if(parseInt(dollars)>=10){
+          dollarsbill = 10
+          final.push(dollarsbill)
+          dollars = dollars-dollarsbill
+        }else if(parseInt(dollars)>=5){
+          dollarsbill = 5
+          final.push(dollarsbill)
+          dollars = dollars-dollarsbill
+        }else if(parseInt(dollars)>=1 && parseInt(dollars)<=4){
+          dollarsbill = 1
+          final.push(dollarsbill)
+          dollars = dollars-dollarsbill
+        }else{
+          final.push(dollars)
+            break;
+        }
+      }
+      for(var i=0;i<final.length; i++){
+        output += `1 ${final[i]} dollar bill, `
+      }
+      if(quarters||dimes||nickels||pennies){
+        output += `${parseInt(quarters)} quarters, ${parseInt(dimes)} dimes, ${parseInt(nickels)} nickles, ${parseInt(pennies)} pennies`
+      }
+      this.setState({result : output})
     }
     
     render(){
     return (
-        <div>
-        <div className="row">
-          <div className="col-md-6 center">
-            <input type="text" ref={(ref) => this.nameTextInput = ref}  onChange={this.updateInput} className="form-control" placeholder="enter value in dollar or cent"></input>
+        <div className="main-container">
+          <div className="row">
+            <div className="col-md-6 center">
+              <h1 className="h1Center">Dollar Change</h1>
+            </div>
           </div>
-        </div>
-        <br></br>
-        <div className="row">
-          <div className="col-md-8 center">
-            <ListCountInfo username={this.state.username}/>
+          <br></br>
+          <div className="row">
+            <div className="col-md-6 center">
+              <input type="text" ref={(ref) => this.valueDollar = ref}  onChange={this.calculateChange} className="form-control" placeholder="enter value in dollar or cent"></input>
+            </div>
           </div>
-        </div>
+          <br></br>
+          <div className="row">
+            <div className="col-md-8 center">
+              <ListCountInfo result={this.state.result}/>
+            </div>
+          </div>
         </div>
       );
     }
@@ -104,7 +108,7 @@ class ListCountInfo extends React.Component {
     return (
       <div className="row">
         <div className="col-md-8 center">
-          <p className="pCenter">Your Change is {this.props.username} </p>
+          <p className="pCenter">Your Change is {this.props.result} </p>
         </div>
       </div>
     );
